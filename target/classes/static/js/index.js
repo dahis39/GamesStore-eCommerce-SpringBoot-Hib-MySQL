3326985dbbc576671ef3177bc53d205f452fbd47
dahis39 <smon_lucia@Hotmail.com>
2016-07-26 08:45:19 -0700

    hide/show carousel buttons on the photo of products in index page.

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
     $('#videoCarousel').find('.item').first().addClass('active');
     $('#videoCarousel').find('.item').css("height","500px");
+
+    $(".carousel-show-button").click(function(){
+        $(".collapse").collapse('show');
+    });
+    $(".carousel-hide-button").click(function(){
+        $(".collapse").collapse('hide');
+    });
 });

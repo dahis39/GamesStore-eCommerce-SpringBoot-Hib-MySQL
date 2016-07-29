cdf78d814cca6d923cbcf65d9f709560b858b6bb
dahis39 <smon_lucia@Hotmail.com>
2016-07-28 17:25:10 -0700

    bundle description div added to index page.

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     $('#videoCarousel').find('.item').first().addClass('active');
     $('#videoCarousel').find('.item').css("height","500px");
 
-    $(".carousel-show-button").click(function(){
+    $("#carousel-show-button").click(function(){
         $(".collapse").collapse('show');
     });
-    $(".carousel-hide-button").click(function(){
+    $("#carousel-hide-button").click(function(){
         $(".collapse").collapse('hide');
     });
 });
